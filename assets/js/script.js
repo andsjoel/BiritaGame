5dbb9e9e40660b7e9bf369fec0f1e0844727a0c7
ANDERSON JOEL <91640303+domnquo@users.noreply.github.com>
2021-12-12 13:18:03 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
-function createCard(cardContent, list) {
-    const card = document.createElement('li')
-    card.innerHTML = `<div class="front">
+function createCard(cardContent, cardDesk) {
+  const divCard = document.createElement('div')
+  divCard.innerHTML = `<div class="front">
+                      <div class="front__inside">
                         <h3>${cardContent.title}</h3>
                         <p>${cardContent.task}</p>
                       </div>
-                      <div class="back">  
-                      </div>`
-    card.addEventListener('click', () => {
-      const cardLi = document.querySelector('.card-desk li')
-      if(cardLi.className.includes('flip')){
-        displayRandomCard()
-      } else {
-        cardLi.classList.add('flip')
-      }
-    })
-    list.appendChild(card)
-  }
-  
-  function displayRandomCard() {
-    const cardDeskList = document.querySelector('.card-desk ul')
-    cardDeskList.innerHTML = ''
-    const randomIndex = getRandomNum(0, cards.length - 1)
-    const selectedCard = cards[randomIndex]
-    createCard(selectedCard, cardDeskList)
-  }
-  
-  function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  const btnImage = document.getElementById('getRandomCard')
-  
-  btnImage.addEventListener('click', displayRandomCard)
-  
-  
+                    </div>
+                    <div class="back">  
+                    </div>`
+  cardDesk.appendChild(divCard)
+  divCard.className = "card"
+  divCard.addEventListener('click', () => {
+    const card = document.querySelector('.card-desk .card')
+    if (card.className.includes('flip')) {
+      card.style.animation = "drop_card 1s"
+      setTimeout(displayRandomCard, 1000)
+    } else {
+      card.classList.add('flip')
+    }
+  })
+}
+
+function displayRandomCard() {
+  const cardDesk = document.querySelector('.card-desk')
+  cardDesk.innerHTML = ''
+  const randomIndex = getRandomNum(0, cards.length - 1)
+  const selectedCard = cards[randomIndex]
+  createCard(selectedCard, cardDesk)
+}
+
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const btnImage = document.getElementById('getRandomCard')
+
+btnImage.addEventListener('click', displayRandomCard)
+
