d464e33f5331c9459b0831601ed71b8045dcf591
ANDERSON JOEL <91640303+domnquo@users.noreply.github.com>
2021-12-13 12:00:26 -0300

    Update cards.js

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,21 +1,35 @@
 const cards = [
-    {
+  {
     title: 'Mochilão',
     task: 'Quem já saiu do Brasil beba para comemorar.',
     doses: '1'
-    },
-    {
+  },
+  {
+    title: 'Clone',
+    task: 'Escolha alguém para fazer tudo que você fizer durante 3 rodadas.',
+    doses:''
+  }, 
+  {
     title: 'Banheirão',
     task: 'Leve alguém para o banheiro. Agora para o que...... ',
     doses: '4'
-    }, 
-    {
+  },
+  {
+    title: 'Mão Boba',
+    task: 'Mão bem boba no amiguinho da direita.',
+    doses: '2'
+  },
+  {
     title: 'Admirador',
     task: 'Escolha uma pessoa que deverá ficar em pé e usar uma venda. Qualquer pessoa poderá dar um selinho nela.',
     doses: ''
-    }
-  
-  ]
-  
-  
-  
+  },
+  {
+    title: 'Abusado',
+    task: 'Beba 1 dose para cada pessoa que já transou da roda.',
+    doses: ''
+  }
+
+]
+
+
