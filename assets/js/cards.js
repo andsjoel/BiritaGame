290915873dd25cf591583f7ff67f3f016a6617c9
ANDERSON JOEL <91640303+domnquo@users.noreply.github.com>
2022-01-13 11:21:52 -0300

    Update cards.js

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,4 +1,6 @@
 const cards = [
+  
+// Cards Normais
   {
     title: 'Mochilão',
     task: 'Quem já saiu do Brasil bebe para comemorar.',
@@ -10,21 +12,274 @@ const cards = [
     doses:''
   },
   {
-    title: 'Mão Boba',
-    task: 'Mão bem boba no amiguinho da direita.',
-    doses: '2'
+    title: 'Just drink',
+    task: 'Beba. Fim.',
+    doses: '3'
   },
   {
+    title: 'Just drink',
+    task: 'Beba. Fim.',
+    doses: '1'
+  },
+    {
+    title: 'Just drink',
+    task: 'Beba. Fim.',
+    doses: '2'
+  },
+    {
+    title: 'Jogo do Pi',
+    task: 'Escolha um multiplicador. Todos devem contar e trocar o multiplicador por Pi, até alguém errar.',
+    doses: '1'
+  },
+    {
+    title: 'Indicação',
+    task: 'Indique alguém. Ele deverá escolher alguém para beber.',
+    doses: '1'
+  },
+    {
+    title: 'Eca',
+    task: 'Heteros? Bebam!',
+    doses: '1'
+  },
+    {
+    title: 'Viva',
+    task: 'Gays brindem e bebem.',
+    doses: '1'
+  },
+    {
+    title: 'Braço direito',
+    task: 'Seu amigo da direita escolhe quem abre a próxima carta.',
+    doses: ''
+  },
+    {
+    title: 'Sorte ou azar?',
+    task: 'Escolha alguém para jogar 3 vezes seguidas.',
+    doses: ''
+  },
+    {
+    title: 'Lembra de alguma carta?',
+    task: 'Você pode escolher qualquer carta que lembrar de cabeça para usar neste momento.',
+    doses: ''
+  },
+    {
+    title: 'Use sua imaginação',
+    task: 'Invente a carta que quiser.',
+    doses: ''
+  },
+    {
+    title: 'Deus',
+    task: 'Por uma rodada você pode alterar os desafios da forma que bem quiser.',
+    doses: ''
+  },
+    {
+    title: 'Pausa',
+    task: 'Jogo pausado. Façam o que tiver que fazer e voltamos em 5 minutinhos.',
+    doses: ''
+  },
+    {
+    title: 'O mestre mandou',
+    task: 'Faça algo que todos deverão repetir. Sinta-se livre rs.',
+    doses: ''
+  },
+    {
+    title: 'Conte-nos',
+    task: 'Conte o lugar mais estranho que já praticou atos sexuais.',
+    doses: ''
+  },
+    {
+    title: 'Happy Birthday',
+    task: 'Aniversariantes do mês bebem para comemorar.',
+    doses: '1'
+  },
+    {
+    title: 'Novinhos',
+    task: 'Os 2 mais novos da roda devem beber.',
+    doses: '1'
+  },
+    {
+    title: 'Pedido negado',
+    task: 'Você tem direito a negar algo.',
+    doses: ''
+  },
+    {
+    title: 'O mais safado',
+    task: 'Diga quem é a pessoa da roda mais safada. Ela bebe.',
+    doses: '1'
+  },
+    {
+    title: 'Pense bem',
+    task: 'Se quiser, você pode trocar de lugar.',
+    doses: ''
+  },
+    {
+    title: 'Planejamento',
+    task: 'Mude quem quiser de lugar. Hora da reforma.',
+    doses: ''
+  },
+    {
+    title: 'Lindeza',
+    task: 'Diga quem é a mulher mais bonita da roda.',
+    doses: ''
+  },
+    {
+    title: 'Lindeza',
+    task: 'Diga quem é o homem mais bonito da roda.',
+    doses: ''
+  },
+    {
+    title: 'PPP',
+    task: 'Quem você pega, pensa e passa?',
+    doses: '2'
+  },
+    {
+    title: 'Questão de gosto',
+    task: 'Diga quem é o mais amorzinho. Quem é o mais chato. E quem não presta.',
+    doses: '1'
+  },
+    {
+    title: 'Tira tudo',
+    task: 'Escolha alguém. Se ele decidir tirar todas peças de roupas, todos da roda deverão tirar.',
+    doses: ''
+  },
+  
+  
+//Cards lets get naked
+  
+  {
+    title: 'Cabide',
+    task: 'Tire 1 peça de roupa.',
+    doses: ''
+  },
+    {
+    title: 'Cabide',
+    task: 'Tire 1 peça de roupa.',
+    doses: '2'
+  },
+    {
+    title: 'Cabide',
+    task: 'Tire 1 peça de roupa.',
+    doses: '3'
+  },
+    {
+    title: 'Cabide',
+    task: 'Tire 2 peça de roupa.',
+    doses: ''
+  },
+    {
+    title: 'Cabide',
+    task: 'Tire 2 peça de roupa.',
+    doses: '2'
+  },
+    {
+    title: 'Cabide',
+    task: 'Tire 2 peça de roupa.',
+    doses: '3'
+  },
+    {
+    title: 'Dobrar roupa',
+    task: 'Escolha alguém para tirar 1 peça de roupa.',
+    doses: ''
+  },
+    {
+    title: 'Passar roupa',
+    task: 'Escolha alguém para tirar 2 peças de roupa.',
+    doses: '2'
+  },
+    {
+    title: 'Hora de lavar roupa',
+    task: 'Escolha alguém para tirar todas as peças de roupas.',
+    doses: '5'
+  },
+    {
+    title: 'Máquina de lavar',
+    task: 'Escolha 2 pessoas para tirar todas as peças de roupas menos uma.',
+    doses: '5'
+  },
+  
+//Cards Ousadas
+  
+    {
     title: 'Admirador',
     task: 'Escolha uma pessoa que deverá ficar em pé e usar uma venda. Qualquer pessoa poderá dar um selinho nela.',
     doses: ''
   },
-  {
+    {
     title: 'Abusado',
     task: 'Beba 1 dose para cada pessoa que já transou da roda.',
     doses: ''
-  }
+  },
+    {
+    title: 'Hmmmm',
+    task: 'Diga quem da roda você daria uma mamada.',
+    doses: ''
+  },
+    {
+    title: 'Vamos descobrir',
+    task: 'Escolha alguém. Ele deverá dizer com quem da roda transaria hoje.',
+    doses: ''
+  },
+      {
+    title: 'Aliviado',
+    task: 'Quem praticou atos sexuais esta semana deve beber.',
+    doses: '1'
+  },
+    {
+    title: 'Boiolinha',
+    task: 'Beba 1 dose para cada pessoa da roda que você já boiolou.',
+    doses: '1'
+  },
+    {
+    title: 'Emocionado',
+    task: 'Vamos deixar as coisas obvias? Dê um selinho no seu crush da noite.',
+    doses: ''
+  },
+    {
+    title: 'Vale beijo',
+    task: 'Beije quem quiser. A pessoa só pode recusar se tiver uma desculpa válida.',
+    doses: ''
+  },
+    {
+    title: 'Trabalho em equipe',
+    task: 'Todos da roda devem te dar um selinho. Para cada pessoa da roda que não der, TODOS devem beber 10 doses.',
+    doses: ''
+  },
+    {
+    title: 'Banheirão',
+    task: 'Leve alguém para o banheiro.',
+    doses: ''
+  },
+    {
+    title: 'Banheirão amigo',
+    task: 'Escolha 3 pessoas para irem ao banheiro juntos.',
+    doses: ''
+  },
+    {
+    title: 'Hoje não',
+    task: 'Diga quem você não pegaria da roda. Se não tiver ninguém, dê um selinho em todos para provar.',
+    doses: '4'
+  },
+    {
+    title: 'Triplo',
+    task: 'Selinho triplo. Escolha mais 2.',
+    doses: '4'
+  },
 
+//Cards +18
+    {
+    title: 'Mão Boba',
+    task: 'Mão bem boba no amiguinho da direita.',
+    doses: '2'
+  },
+    {
+    title: 'Cavalgada',
+    task: 'Escolha alguém para ficar deitado de barriga pra cima. Escolha outro alguém que deve simular uma boa cavalgada nele.',
+    doses: '3'
+  },
+
+  
+  
+//Cards Eu Nunca
+  
 ]
 
 
